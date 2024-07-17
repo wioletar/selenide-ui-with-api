a42e707fd06431d88fddd70a886af68f8c5214b6
wrudzka <w.rudzka1@gmail.com>
2024-07-17 12:00:30 +0200

    alias added to SelenideElement.

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -7,14 +7,14 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class HomePage {
-    public SelenideElement consentButton = $("button.fc-cta-consent p");
-    public SelenideElement signupLoginButton = $("a[href='/login']");
-    public SelenideElement loggedInButton = $x("//ul//a[contains(text(),'Logged')]");
-    public SelenideElement deleteAccountButton = $x("//ul//a[contains(text(),'Delete')]");
+
+    public SelenideElement consentButton = $("button.fc-cta-consent p").as("Consent button");
+    public SelenideElement signupLoginButton = $("a[href='/login']").as("Signup / Login");
+    public SelenideElement loggedInButton = $x("//ul//a[contains(text(),'Logged')]").as("Logged in as");
+    public SelenideElement deleteAccountButton = $x("//ul//a[contains(text(),'Delete')]").as("Delete Account");
 
     @Step
-    public HomePage clickDeleteAccountButton() {
+    public void deleteAccount() {
         deleteAccountButton.click();
-        return this;
     }
 }
