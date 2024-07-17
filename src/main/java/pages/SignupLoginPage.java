36fe8750ebcc3945ddd78c943de42fb5ca60444f
wrudzka <w.rudzka1@gmail.com>
2024-07-17 12:04:41 +0200

    methods refactor

--- src/main/java/pages/SignupLoginPage.java
+++ src/main/java/pages/SignupLoginPage.java
@@ -9,19 +9,20 @@ import static com.codeborne.selenide.Selenide.$;
 public class SignupLoginPage {
 
     public SelenideElement newUserSignupHeaderText = $(".signup-form h2");
+    public SelenideElement loginHeaderText = $(".login-form h2");
     public SelenideElement signupName = $("form[action='/signup'] input[type='text']");
     public SelenideElement signupEmail = $("form[action='/signup'] input[type='email']");
     public SelenideElement signupButton = $("button[data-qa='signup-button']");
 
     @Step
-    public SignupLoginPage inputSignupName(User user) {
-        signupName.setValue(user.getName());
+    public SignupLoginPage inputSignupName(String name) {
+        signupName.setValue(name);
         return this;
     }
 
     @Step
-    public SignupLoginPage inputSignupEmail(User user) {
-        signupEmail.setValue(user.getEmail());
+    public SignupLoginPage inputSignupEmail(String email) {
+        signupEmail.setValue(email);
         return this;
     }
 }
