094ccb207a264762f306836ec1bbbb87fb158b2f
wrudzka <w.rudzka1@gmail.com>
2024-07-17 12:03:31 +0200

    SelenideElements refactor + methods refactor + fillFormCorrectly method added.

--- src/main/java/pages/RegisterPage.java
+++ src/main/java/pages/RegisterPage.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import model.User;
-import org.openqa.selenium.By;
 
 import java.util.Random;
 
@@ -14,142 +13,150 @@ import static com.codeborne.selenide.Selenide.$$;
 public class RegisterPage {
 
     Random random = new Random();
+    SignupLoginPage signupLoginPage = new SignupLoginPage();
     public ElementsCollection genderRadioButtons = $$("div.radio-inline input[type='radio']");
     public SelenideElement passwordInput = $("input[type='password']");
     public SelenideElement newsletterCheckbox = $("label[for='newsletter']");
     public SelenideElement offersCheckbox = $("label[for='optin']");
     public SelenideElement createAccountButton = $("button[data-qa='create-account']");
-    public SelenideElement firstNameInput = $(By.id("first_name"));
-    public SelenideElement lastNameInput = $(By.id("last_name"));
-    public SelenideElement yearSelect = $(By.id("years"));
-    public ElementsCollection yearList = $$("select[data-qa='years'] option");
-    public SelenideElement monthsSelect = $(By.id("months"));
-    public ElementsCollection monthList = $$("select[data-qa='months'] option");
-    public SelenideElement daySelect = $(By.id("days"));
-    public ElementsCollection dayList = $$("select[data-qa='days'] option");
-    public SelenideElement countryInput = $(By.id("country"));
+    public SelenideElement firstNameInput = $("#first_name");
+    public SelenideElement lastNameInput = $("#last_name");
+    public SelenideElement yearSelect = $("#years");
+    public SelenideElement monthsSelect = $("#months");
+    public SelenideElement daySelect = $("#days");
+    public SelenideElement countryInput = $("#country");
     public ElementsCollection countryList = $$("select[data-qa='country'] option");
-    public SelenideElement companyInput = $(By.id("company"));
-    public SelenideElement address1Input = $(By.id("address1"));
-    public SelenideElement address2Input = $(By.id("address2"));
-    public SelenideElement stateInput = $(By.id("state"));
-    public SelenideElement cityInput = $(By.id("city"));
-    public SelenideElement zipcodeInput = $(By.id("zipcode"));
-    public SelenideElement mobileNumberInput = $(By.id("mobile_number"));
+    public SelenideElement companyInput = $("#company");
+    public SelenideElement address1Input = $("#address1");
+    public SelenideElement address2Input = $("#address2");
+    public SelenideElement stateInput = $("#state");
+    public SelenideElement cityInput = $("#city");
+    public SelenideElement zipcodeInput = $("#zipcode");
+    public SelenideElement mobileNumberInput = $("#mobile_number");
 
     @Step
-    public RegisterPage clickRandomTitle() {
+    public void clickRandomTitle() {
         int randomItem = random.nextInt(genderRadioButtons.size() - 1);
         genderRadioButtons.get(randomItem).click();
-        return this;
     }
 
     @Step
-    public RegisterPage inputPassword(User user) {
-        passwordInput.setValue(user.getPassword());
-        return this;
+    public void enterPassword(String password) {
+        passwordInput.setValue(password);
     }
 
     @Step
-    public RegisterPage clickNewsletterCheckbox() {
+    public void checkNewsletter() {
         newsletterCheckbox.scrollTo();
         newsletterCheckbox.click();
-        return this;
     }
 
     @Step
-    public RegisterPage clickOffersCheckbox() {
+    public void checkOffers() {
         offersCheckbox.click();
-        return this;
     }
 
     @Step
-    public RegisterPage clickCreateAccountButton() {
+    public void clickCreateAccountButton() {
         createAccountButton.scrollTo();
         createAccountButton.click();
-        return this;
     }
 
     @Step
-    public RegisterPage clickRandomYear() {
+    public void clickRandomYear() {
         yearSelect.scrollTo();
+        ElementsCollection yearList = $$("select[data-qa='years'] option");
         int randomItem = random.nextInt(1, yearList.size() - 1);
         yearSelect.selectOption(randomItem);
-        return this;
     }
 
     @Step
-    public RegisterPage clickRandomMonth() {
+    public void clickRandomMonth() {
+        ElementsCollection monthList = $$("select[data-qa='months'] option");
         int randomItem = random.nextInt(1, monthList.size() - 1);
         monthsSelect.selectOption(randomItem);
-        return this;
     }
 
     @Step
-    public RegisterPage clickRandomDay() {
+    public void clickRandomDay() {
+        ElementsCollection dayList = $$("select[data-qa='days'] option");
         int randomItem = random.nextInt(1, dayList.size() - 1);
         daySelect.selectOption(randomItem);
-        return this;
     }
 
     @Step
-    public RegisterPage clickRandomCountry() {
+    public void clickRandomCountry() {
         int randomItem = random.nextInt(0, countryList.size() - 1);
         countryInput.selectOption(randomItem);
-        return this;
     }
 
     @Step
-    public RegisterPage inputFirstName(User user) {
-        firstNameInput.setValue(user.getFirstName());
-        return this;
+    public void inputFirstName(String firstName) {
+        firstNameInput.setValue(firstName);
     }
 
     @Step
-    public RegisterPage inputLastName(User user) {
-        lastNameInput.setValue(user.getLastName());
-        return this;
+    public void inputLastName(String lastName) {
+        lastNameInput.setValue(lastName);
+
     }
 
     @Step
-    public RegisterPage inputCompany(User user) {
-        companyInput.setValue(user.getCompany());
-        return this;
+    public void inputCompany(String company) {
+        companyInput.setValue(company);
     }
 
     @Step
-    public RegisterPage inputAddress(User user) {
-        address1Input.setValue(user.getAddress());
-        return this;
+    public void inputAddress(String address) {
+        address1Input.setValue(address);
     }
 
     @Step
-    public RegisterPage inputAddress2(User user) {
-        address2Input.setValue(user.getAddress2());
-        return this;
+    public void inputAddress2(String address2) {
+        address2Input.setValue(address2);
     }
 
     @Step
-    public RegisterPage inputState(User user) {
-        stateInput.setValue(user.getState());
-        return this;
+    public void inputState(String state) {
+        stateInput.setValue(state);
     }
 
     @Step
-    public RegisterPage inputCity(User user) {
-        cityInput.setValue(user.getCity());
-        return this;
+    public void inputCity(String city) {
+        cityInput.setValue(city);
     }
 
     @Step
-    public RegisterPage inputZipcode(User user) {
-        zipcodeInput.setValue(user.getZipcode());
-        return this;
+    public void inputZipcode(String zipcode) {
+        zipcodeInput.setValue(zipcode);
     }
 
     @Step
-    public RegisterPage inputMobileNumber(User user) {
-        mobileNumberInput.setValue(user.getMobileNumber());
-        return this;
+    public void inputMobileNumber(String mobileNumber) {
+        mobileNumberInput.setValue(mobileNumber);
+    }
+
+    public void fillFormCorrectly(User user) {
+        signupLoginPage.inputSignupName(user.getName());
+        signupLoginPage.inputSignupEmail(user.getEmail());
+        signupLoginPage.signupButton.click();
+        clickRandomTitle();
+        enterPassword(user.getPassword());
+        clickRandomYear();
+        clickRandomMonth();
+        clickRandomDay();
+        checkNewsletter();
+        checkOffers();
+        inputFirstName(user.getFirstName());
+        inputLastName(user.getLastName());
+        inputCompany(user.getCompany());
+        inputAddress(user.getAddress());
+        inputAddress2(user.getAddress2());
+        clickRandomCountry();
+        inputState(user.getState());
+        inputCity(user.getCity());
+        inputZipcode(user.getZipcode());
+        inputMobileNumber(user.getMobileNumber());
+        clickCreateAccountButton();
     }
 }
